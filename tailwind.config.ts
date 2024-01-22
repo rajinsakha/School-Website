780ffb21b0319cc45ef8645d50bab6d88261f34f
Rajin Sakha <rajinsakha07@gmail.com>
2024-01-22 22:42:01 +0545

    fixing runtime fail

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
 import type { Config } from "tailwindcss"
 
-const { fontFamily } = require("tailwindcss/defaultTheme")
 
 const config = {
   darkMode: ["class"],
@@ -20,9 +19,7 @@ const config = {
       },
     },
     extend: {
-      fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
-      },
+   
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
