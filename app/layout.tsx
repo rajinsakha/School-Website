f4cbe8efdc0b7dfeb765bb70afe463d86901111d
Rajin Sakha <rajinsakha07@gmail.com>
2024-01-22 22:49:51 +0545

    removing-font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-import { Inter as FontSans } from "next/font/google";
-import { cn } from "@/lib/utils";
+// import { Inter as FontSans } from "next/font/google";
+// import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
-
-export const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
 
 export const metadata: Metadata = {
   title: "Next Blog",
@@ -22,21 +17,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-         <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
+      <body className="min-h-screen bg-background font-sans antialiased">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
